da0dd2c27538d6a3340eb876e051937934a9f700
longjintao <longjt1@chinatelecom.cn>
2022-10-19 11:07:43 +0800

    第一次提交，能正常运行的例子

---
+++ src/views/HomeView.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="home">
+    <!--<img alt="Vue logo" src="../assets/logo.png" />-->
+    <img
+      src="https://nklayman.github.io/vue-cli-plugin-electron-builder/hero.png"
+      alt="Vue logo"
+      style="width: 70%"
+    />
+    <HelloWorld msg="Welcome to Your Vue.js + Electron App" />
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+
+@Options({
+  components: {
+    HelloWorld,
+  },
+})
+export default class HomeView extends Vue {}
+</script>
